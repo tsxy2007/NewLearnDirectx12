4b15ae3258f477ad4f5eddea2bcbaca32ba7e239
tsxy2007 <z.tk@163.com>
2021-06-09 22:12:22 +0800

    GameTimer

---
+++ LearnDirectx12/GameTimer.cpp
@@ -0,0 +1,6 @@
+#include "GameTimer.h"
+
+GameTimer::GameTimer()
+{
+
+}
---
+++ LearnDirectx12/GameTimer.h
@@ -0,0 +1,24 @@
+#pragma once
+class GameTimer
+{
+public:
+	GameTimer();
+
+	float TotalTime()const;// 用秒做单位
+	float DeltaTime()const;
+	void Reset(); // 在开始消息循环之前调用
+	void Start(); // 解除暂停
+	void Stop();// 暂停
+	void Tick();
+
+private:
+	double mSecondsPerCount;
+	double mDeltaTime;
+
+	__int64 mBaseTime;
+	__int64 mPasuedTime;
+	__int64 mStopTime;
+	__int64 mPrevTime;
+	__int64 mCurrTime;
+};
+
