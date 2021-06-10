05f7c1f2ade5dbb44cd699e4ba9ae14996965c0d
tsxy2007 <z.tk@163.com>
2021-06-10 19:51:01 +0800

    D3DApp 接口

--- LearnDirectx12/GameTimer.cpp
+++ LearnDirectx12/GameTimer.cpp
@@ -15,3 +15,81 @@ GameTimer::GameTimer()
 	QueryPerformanceFrequency((LARGE_INTEGER*)&countPresec);
 	mSecondsPerCount = 1.f / (double)countPresec;
 }
+
+float GameTimer::TotalTime() const 
+{
+	if (bStop)
+	{
+		return (float)(((mStopTime - mBaseTime) - mPasuedTime) * mSecondsPerCount);
+	}
+	else
+	{
+		return (float)(((mCurrTime - mBaseTime) - mPasuedTime) * mSecondsPerCount);
+	}
+	return 0.f;
+}
+
+float GameTimer::DeltaTime() const
+{
+	return (float)mDeltaTime;
+}
+
+void GameTimer::Reset()
+{
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	mBaseTime = currTime;
+	mPrevTime = currTime;
+	mStopTime = 0;
+	bStop = false;
+}
+
+void GameTimer::Start()
+{
+	__int64 StartTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&StartTime);
+
+	if (bStop)
+	{
+		mPasuedTime += (StartTime - mStopTime);
+		mPrevTime = StartTime;
+
+		mStopTime = 0;
+		bStop = false;
+	}
+}
+
+void GameTimer::Stop()
+{
+	if (!bStop)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+		mStopTime = currTime;
+		bStop = true;
+	}
+}
+
+void GameTimer::Tick()
+{
+	if (bStop)
+	{
+		mDeltaTime = 0.f;
+		return;
+	}
+
+	//获取本帧开始显示的时刻
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	mCurrTime = currTime;
+	//本帧与前一帧的时间差
+	mDeltaTime = (currTime - mPrevTime) * mSecondsPerCount;
+
+	//准备计算本帧与下一帧时间差
+	mPrevTime = currTime;
+	if (mDeltaTime < 0)
+	{
+		mDeltaTime = 0.f;
+	}
+}
